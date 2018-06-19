7341f4ea6f709de6ed4d9bf77acf5b8df5eb97fc
Holden Altaffer <haltaffer@gmail.com>
2018-06-19 04:22:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-    session[:item] = params[:item][:name]
+    session["item"] = params[:item][:name]
     "#{params[:item][:name]}"
   end
 end
